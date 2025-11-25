9c60bf9ab8342062b8a9f3d73d529b8bca630fa7
Tanishque99 <tzaware@asu.edu>
2025-11-24 21:45:46 -0700

    Remove comment on load_synthetic count adjustment
    Removed comment about adjusting counts for laptop capacity.

--- src/run_demo.py
+++ src/run_demo.py
@@ -5,7 +5,6 @@ from init_drivers import init_drivers
 from replayer import replayer
 
 def main():
-    # Adjust counts as your laptop can handle
     load_synthetic(100)
     clean_data()
     init_drivers(10)
